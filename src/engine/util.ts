73bec00d3c55056f6aed4dae6586766c723a5d29
marihachi <marihachi0620@gmail.com>
2026-09-21 22:16:38 +0900

    ドキュメント更新

--- src/engine/util.ts
+++ src/engine/util.ts
@@ -1,4 +1,4 @@
-// このアプリの題材に属さない、汎用的な小さな関数
+// このアプリのどの責務にも属さない、汎用的な小さな関数
 
 export function isObject(v: unknown): v is Record<string, unknown> {
   return typeof v === 'object' && v !== null && !Array.isArray(v);
